90b33ce1dc34c065234beceb32d3694e5ec7df5f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-03 13:33:30 +0100

    Configured houdini for dnd5e api

---
+++ houdini.config.js
@@ -0,0 +1,14 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "watchSchema": {
+        "url": "https://www.dnd5eapi.co/graphql",
+        "interval": null,
+    },
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
