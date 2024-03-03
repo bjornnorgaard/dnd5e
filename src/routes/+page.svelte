05ec15b8aff9a582aec9a27cfbc3e6fd2118cf9b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-03 13:50:07 +0100

    Initial graphql usage working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+    import { graphql } from "$houdini";
+
+    let filter: number | null = null;
+
+    const searchStore = graphql(`
+        query Search($armorClass: IntFilter){
+            monsters(limit: 5, armor_class: $armorClass) {
+                name, challenge_rating, armor_class {
+                    desc, armor {
+                        armor_class {
+                            base
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    function doSearch() {
+        searchStore.fetch({variables: {armorClass: filter}})
+    }
+
+</script>
+
+<form on:submit|preventDefault={() => doSearch()}>
+    <label for="name">Name</label>
+    <input name="name" type="number" autocomplete="off">
+</form>
+
+<pre>{JSON.stringify($searchStore, null, 2)}</pre>
