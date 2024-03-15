57e44b7f2c0ca069db308bc89207e112ceae2b64
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-15 16:34:22 +0100

    Formatting +server.ts

--- src/routes/api/monsters/[filter]/+server.ts
+++ src/routes/api/monsters/[filter]/+server.ts
@@ -1,7 +1,7 @@
 import monsters from "../monsters.json";
 import { json } from "@sveltejs/kit";
 
-export const GET = ({params}) => {
+export const GET = ({ params }) => {
     const data = monsters.results;
     const res = data.filter(m => m.name.toLowerCase().includes(params.filter.toLowerCase()));
     return json(res);
