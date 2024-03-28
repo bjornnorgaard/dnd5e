1c1128876ca2874a4ca09cf87485759ea83c1261
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-28 18:17:13 +0100

    Updated page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
     import PageSection from "$lib/components/PageSection.svelte";
 </script>
 
-<PageWrapper title="Home">
-    <PageSection title="Quick Actions">
+<PageWrapper title="D&D by Bear">
+    <PageSection title="Quick Actions" desc="These are just for me while debugging">
         <button class="btn variant-filled-primary" on:click={async () => await addDefaultPlayers()}>Add Default Players</button>
     </PageSection>
 </PageWrapper>
