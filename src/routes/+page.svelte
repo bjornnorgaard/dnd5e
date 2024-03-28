299540130706ef675de0dd8cbe3932f9efa01309
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-28 18:27:40 +0100

    Fixed typo in home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
     import PageWrapper from "$lib/components/PageWrapper.svelte";
 </script>
 
-<PageWrapper title="D&D by Bear" desc="An nice onboarding tutorial is coming...">
+<PageWrapper title="D&D by Bear" desc="A nice onboarding tutorial is coming...">
     <p>For now go to one of the other sections and try to figure it out - good luck.</p>
 </PageWrapper>
