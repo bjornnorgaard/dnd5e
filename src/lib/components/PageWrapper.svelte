b80970fffe66c4a53cbebf39d9c96de709313091
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-03-29 15:06:51 +0100

    Updated statblock sidebar

--- src/lib/components/PageWrapper.svelte
+++ src/lib/components/PageWrapper.svelte
@@ -5,27 +5,14 @@
     export let desc: string | null = null;
 </script>
 
-<div class="mx-auto max-w-screen-md space-y-8 py-[3vw] px-4">
+<div class="mx-auto max-w-screen-md space-y-8 py-[2vw] px-4">
     <AppBar background="bg-transparent" padding="">
         <svelte:fragment slot="headline">
             <h1 class="h1 font-bold">{title}</h1>
             {#if desc}
                 <p>{desc}</p>
             {/if}
-            <!--{#if getBreadcrumbs($page.url.pathname).length > 1}-->
-            <!--    <ol class="breadcrumb pt-2">-->
-            <!--        {#each getBreadcrumbs($page.url.pathname) as crumb, i}-->
-            <!--            {#if i < getBreadcrumbs($page.url.pathname).length - 1}-->
-            <!--                <li class="crumb"><a class="anchor" href={crumb.route}>{crumb.label}</a></li>-->
-            <!--                <li class="crumb-separator" aria-hidden="true">/</li>-->
-            <!--            {:else}-->
-            <!--                <li class="crumb truncate">{crumb.label}</li>-->
-            <!--            {/if}-->
-            <!--        {/each}-->
-            <!--    </ol>-->
-            <!--{/if}-->
         </svelte:fragment>
     </AppBar>
-
     <slot/>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,13 @@
     import "../app.pcss";
     import { AppShell, storePopup } from '@skeletonlabs/skeleton';
     import NavSide from "$lib/components/NavSide.svelte";
-    import { cubicIn } from "svelte/easing";
-    import CreatureStats from "$lib/components/CreatureStats.svelte";
     import { statblock } from "$lib/stores/statblock";
     import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
     import { slide } from "svelte/transition";
     import NavBottom from "$lib/components/NavBottom.svelte";
     import { inject } from '@vercel/analytics'
     import { dev } from "$app/environment";
+    import CreatureStatblock from "$lib/components/CreatureStatblock.svelte";
 
     storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
@@ -29,8 +28,8 @@
 
     <svelte:fragment slot="sidebarRight">
         {#if $statblock.isOpen}
-            <div class="w-96 p-4" in:slide={{axis: "x", duration: 100}} out:slide={{axis: "x", easing: cubicIn}}>
-                <CreatureStats creature={$statblock.creature}/>
+            <div class="w-96 p-4" in:slide={{axis: "x", duration: 300}} out:slide={{axis: "x", duration: 300}}>
+                <CreatureStatblock creature={$statblock.creature}/>
             </div>
         {/if}
     </svelte:fragment>
