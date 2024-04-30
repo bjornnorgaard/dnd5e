a2eea8c7fc97d53ec1d76326c66c9c7144ec078a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2024-04-30 09:02:24 +0200

    Put encounter list over player list

--- src/routes/combat/+page.svelte
+++ src/routes/combat/+page.svelte
@@ -22,13 +22,34 @@
     }
 
     async function createPlayer() {
-        const player = newPlayer({ name: "Unnamed Player", armorClass: 10, hitPoints: 10, dexterity: 0 })
+        const player = newPlayer({ name: "Unnamed Player", armorClass: 10, hitPoints: 10, dexterity: 0 });
         await db.creatures.add(player, player.id);
         await goto(routes.combat_creature(player.id));
     }
 </script>
 
 <PageWrapper title="Combat" desc="Create encounters and use initiative tracker">
+    <PageSection title="Encounters" desc="Encounters are a collection of monsters and players">
+        <Table>
+            <TableHead>
+                <th>Encounter</th>
+                <th>Creatures</th>
+            </TableHead>
+            <TableBody>
+                {#if $encounters}
+                    {#each $encounters as e}
+                        <tr>
+                            <td><a class="anchor" href="{routes.combat_encounter(e.id)}">{e.title}</a></td>
+                            <td>{e.creatureIds.length}</td>
+                        </tr>
+                    {/each}
+                {/if}
+            </TableBody>
+        </Table>
+        <div>
+            <button on:click="{async () => await createEncounter()}" class="btn btn-sm variant-filled-primary">New Encounter</button>
+        </div>
+    </PageSection>
     <PageSection title="Players" desc="Players are characters that can be added to encounters">
         <Table>
             <TableHead>
@@ -41,7 +62,7 @@
                 {#if $players}
                     {#each $players as p}
                         <tr>
-                            <td><a class="anchor" href={routes.combat_creature(p.id)}>{p.name}</a></td>
+                            <td><a class="anchor" href="{routes.combat_creature(p.id)}">{p.name}</a></td>
                             <td>{p.current_hit_points}/{p.hit_points}</td>
                             <td>{p.armor_class}</td>
                             <td>{p.dexterity}</td>
@@ -51,28 +72,7 @@
             </TableBody>
         </Table>
         <div>
-            <button on:click={async () => await createPlayer()} class="btn btn-sm variant-filled-primary">New Player</button>
-        </div>
-    </PageSection>
-    <PageSection title="Encounters" desc="Encounters are a collection of monsters and players">
-        <Table>
-            <TableHead>
-                <th>Encounter</th>
-                <th>Creatures</th>
-            </TableHead>
-            <TableBody>
-                {#if $encounters}
-                    {#each $encounters as e}
-                        <tr>
-                            <td><a class="anchor" href={routes.combat_encounter(e.id)}>{e.title}</a></td>
-                            <td>{e.creatureIds.length}</td>
-                        </tr>
-                    {/each}
-                {/if}
-            </TableBody>
-        </Table>
-        <div>
-            <button on:click={async () => await createEncounter()} class="btn btn-sm variant-filled-primary">New Encounter</button>
+            <button on:click="{async () => await createPlayer()}" class="btn btn-sm variant-filled-primary">New Player</button>
         </div>
     </PageSection>
 </PageWrapper>
